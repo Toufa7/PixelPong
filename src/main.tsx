8a5ebc3a87a48ba8790ab3ab117d43127ceecf59
Toufa7 <otoufah@1337.student.ma>
2023-09-19 20:16:58 +0100

    Starting Profil Page

--- src/main.tsx
+++ src/main.tsx
@@ -9,13 +9,13 @@ import ReactDOM from 'react-dom/client'
 import NavBar from './Pages/addons/NavBar';
 import Stars from './Pages/addons/Stars';
 // import LoginPage from './Pages/loginPage/LoginPage';
-import Dashboard from './Pages/dashboardPage/Dashboard';
+import ProfilPage from './Pages/profilPage/profilPage';
 
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Dashboard/>
+    <ProfilPage/>
     {/* <NavBar/> */}
     {/* <Stars/> */}
   </React.StrictMode>,
