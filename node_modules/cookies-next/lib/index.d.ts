95cbf64c741fbc039421a4bacdff78c370004a17
ibrnad <57199774+ibranad@users.noreply.github.com>
2023-10-01 12:26:39 +0100

    node_mode removed

--- node_modules/cookies-next/lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-import type { OptionsType, TmpCookiesObj, CookieValueTypes } from './types';
-export { CookieValueTypes } from './types';
-export declare const getCookies: (options?: OptionsType) => TmpCookiesObj;
-export declare const getCookie: (key: string, options?: OptionsType) => CookieValueTypes;
-export declare const setCookie: (key: string, data: any, options?: OptionsType) => void;
-export declare const deleteCookie: (key: string, options?: OptionsType) => void;
-export declare const hasCookie: (key: string, options?: OptionsType) => boolean;
