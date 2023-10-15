585558090ca2aa8d58a61298aba219b988295b8c
ibranad <57199774+ibranad@users.noreply.github.com>
2023-10-15 11:53:44 +0100

    CI: working with maps in MessagInput componenet instead of array

--- Frontend/src/Pages/chatPage/components/messageInput.tsx
+++ Frontend/src/Pages/chatPage/components/messageInput.tsx
@@ -1,11 +1,11 @@
+import '../chatPage.scss'
 import { useState, useRef, useEffect, useContext } from 'react'
-import io from 'socket.io-client';
+import { chatSocketContext } from './socketContext'
+import { useMap } from "@uidotdev/usehooks";
+import Send from '../../../assets/images/send.svg'
+import axios from 'axios';
 import MessageComponent from './messageComponenet'
 import MessageRightComponenet from './messageRightComponenet ';
-import '../chatPage.scss'
-import Send from '../../../assets/images/send.svg'
-import { chatSocketContext } from './socketContext'
-import axios from 'axios';
 
 // Class chatAgent responsible for defining the properties of each person onthe conversation
 interface chatAgent
@@ -18,8 +18,12 @@ interface chatAgent
     timestamp: string;
 }
 
+//<*-----------------------------------------------------------------------------------------------------------------------------------*>
+
+//This componenet is responsible for displaying a conversation, it take an array of messages  
 const Conversation = (props: any) =>
 {
+
     const mesaageEndRef = useRef(null);
 
     //Handle scroll to bottom
@@ -42,6 +46,12 @@ const Conversation = (props: any) =>
     );
 }
 
+//<*-----------------------------------------------------------------------------------------------------------------------------------*>
+
+
+//This componenet is responsible for getting a sending a message
+//getting old messages, and preparing messages array to be displayed
+
 const messageInput = (props: any) => {
 
     //Refering to the dummy div
@@ -53,7 +63,8 @@ const messageInput = (props: any) => {
     //Creating the messages array to be rendred
     const [messagesArr, setNewMessage] = useState<chatAgent[]>([]);
     const [oldMessages, setOldMessages] = useState(new Map<string, chatAgent>());
-    
+    let map = useMap();
+
     useEffect(() => {
         axios
             .get(`http://localhost:3000/chat/getOldMessages/${props.Receiver.id}`, { withCredentials: true })
@@ -67,16 +78,18 @@ const messageInput = (props: any) => {
     
     
     const fillMap = (axiosResponse: any) => {
+
+        map.clear();
         
         let molLmessageId: any = 'n/a';
         let molLmessage: any = 'n/a';
         let molMsgPic: any = 'n/a';
         let molMsgSide: number = 0;
-
-
+        
+        
         for (let i: number = 0; i < axiosResponse.length; i++)
         {
-
+            
             if (axiosResponse[i].senderId == props.Sender.id)
             {
                 molLmessageId = axiosResponse[i].senderId;
@@ -101,17 +114,20 @@ const messageInput = (props: any) => {
                 timestamp: axiosResponse[i].createdAt,
             }
 
-            setOldMessages(oldMessages.set(axiosResponse[i].id, tmpMsgObj));
-            setNewMessage(prevMessagesArr => [...prevMessagesArr, tmpMsgObj]);
+            // setOldMessages(oldMessages.set(axiosResponse[i].id, tmpMsgObj));
+            map.set(axiosResponse[i].id, tmpMsgObj);
+            // setNewMessage(prevMessagesArr => [...prevMessagesArr, tmpMsgObj]);
         }
         
     };
+
+    // console.log("use hook map is : ", map)
     
-    function makeid(length: number) {
+    function makeid(length: number) :string {
         
-        let result = '';
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        const charactersLength = characters.length;
+        let result: string = '';
+        const characters: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const charactersLength: number = characters.length;
         let counter = 0;
         while (counter < length) {
           result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -145,7 +161,10 @@ const messageInput = (props: any) => {
             message: newMessage.message,
             timestamp: "n/a",
         }
-        setNewMessage(prevMessagesArr => [...prevMessagesArr, tmpMsgObj]);
+        // setNewMessage(prevMessagesArr => [...prevMessagesArr, tmpMsgObj]);
+        // setOldMessages(oldMessages.set(makeid(37), tmpMsgObj));
+        map.set(makeid(37), tmpMsgObj);
+
     }
     
 
@@ -176,15 +195,19 @@ const messageInput = (props: any) => {
                 timestamp: "n/a",
             }
             firstRef.current.value = '';
-            setNewMessage(prevMessagesArr => [...prevMessagesArr, tmpMsgObj]);
+            // setOldMessages(oldMessages.set(makeid(37), tmpMsgObj));
+            map.set(makeid(37), tmpMsgObj);
             handleNewMessage(tmpMsgObj);
+            // setNewMessage(prevMessagesArr => [...prevMessagesArr, tmpMsgObj]);
         }
         //N'oublier pas d'envoyer messagesArr a Conversation composant
     }
 
+    // console.log("Map is : ", map)
+
     return (
     <>
-        <Conversation MessagesArr={messagesArr}/>
+        <Conversation MessagesArr={Array.from(map.values())}/>
         <div className="messageInput">
             <form className='messageform' onSubmit={onSubmitHandler}>
                 <input className='messageInputBox' ref={firstRef} placeholder='Type your message here ...'></input>
