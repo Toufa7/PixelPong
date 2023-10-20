d0a64f6be027f68f2086073ed05eb73756674fe8
Omar Toufah <otoufah@e1r5p2.1337.ma>
2023-10-20 20:15:24 +0100

    CI: Groups Management

--- Frontend/src/Pages/XnewGroupChat/components/GrpsocketContext.tsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-import { Socket, io } from "socket.io-client";
-
-export const grpSocket : Socket = io("ws://localhost:3000/groupchat", { withCredentials: true });
-export const grpSocketContext = createContext<Socket>(grpSocket);
