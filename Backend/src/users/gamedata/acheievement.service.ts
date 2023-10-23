75750aeb2f20993fe3596c655d45fc8a985cb129
Mohamed-Amellal <mamellal@student.1337.ma>
2023-10-23 13:03:10 +0100

    Add achievement in database and start set it in backend

--- Backend/src/users/gamedata/acheievement.service.ts
+++ Backend/src/users/gamedata/acheievement.service.ts
@@ -1,16 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { Type } from '@prisma/client';
 import { PrismaService } from 'src/auth/prisma.service';
 
 @Injectable()
 export class achievementService {
   constructor(private prisma: PrismaService) {}
-  // async gameAchivement(UserId: string, wins: )
-  // {
-
-  // }
-  // async relationAchievement(){
-
-  // }
-}
-
+  async createAchievement(userId: string, type: Type){
+    let achievement = await this.prisma.achievements.findFirst({
+      where: {
+        userId: userId,
+      },
+    })
+    if(!achievement)
+  {
+    achievement = await this.prisma.achievements.create({
+      data: {
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+        achievementType: [type],
+      },
+    });
+    return achievement;
+  }
+  }
+  async updateAchievement(userId: string, type: Type){
+    const achievement = await this.prisma.achievements.findFirst({
+      where: {
+        userId: userId,
+      },
+    })
+    const update = await this.prisma.achievements.update({
+      where: {
+        id: achievement.id,
+        },
+      data: {
+        achievementType: {
+          set: [...achievement.achievementType, type],
+        }
+      }
+  })
+}}
 //chekc with khalil how we will do this
+  
