0a325523ad8ea44260bf48213f1e4c0cc80c0b8f
Ayoub Bensguia <abensgui@e1r4p5.1337.ma>
2023-10-24 17:48:09 +0100

    before pull

--- Backend/src/dto/CreateGroupchat.dto.ts
+++ Backend/src/dto/CreateGroupchat.dto.ts
@@ -1,8 +1,17 @@
+import { IsNotEmpty, IsString } from "class-validator";
 
 
 export class CreateGroupchatDto {
-
+    @IsNotEmpty()
+    @IsString()
     namegb : string;
+
+    @IsNotEmpty()
+    @IsString()
     grouptype: string;
+
+    @IsNotEmpty()
+    @IsString()
     password? : string;
 }
+
