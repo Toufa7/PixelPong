b669c4f46cc54e4566a8a3e2c4e7d9efcd3ab74b
Mohamed Amellal <mamellal@c3r4p6.1337.ma>
2023-11-13 15:18:07 +0100

    rm realtion Folder

--- Backend/src/users/users.module.ts
+++ Backend/src/users/users.module.ts
@@ -4,11 +4,8 @@ import { UsersController } from './users.controller';
 import { PrismaService } from 'src/auth/prisma.service';
 import { achievementService } from './gamedata/acheievement.service';
 import { HistoryService } from './gamedata/history.service';
-// import { RelationModule } from '../relation/relation.module';
-// import { RelationService } from '../relation/relation.service';
 
 @Module({
-  // imports: [RelationModule],
   providers: [ UsersService, PrismaService,HistoryService, achievementService],
   controllers: [UsersController],
   exports: [UsersService],
